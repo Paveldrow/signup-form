6088433ea1550658e8a641b5705371ac22f3a182
Pavel Drow <paveldrow@yandex.ru>
2021-04-13 17:42:47 +0300

    done project without form validation on js

---
+++ script/main.js
@@ -0,0 +1,25 @@
+// const email = document.querySelector('.form__email');
+// const form = document.querySelector('.form');
+
+
+
+// form.addEventListener("submit", function (event) {
+//   const reg = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
+//   if (email.value) {
+
+//     // Если поле невалидно, отображается пользовательское
+//     // сообщение об ошибке.
+//     email.classList.remove('input--valid')
+//     email.classList.add('input--error')
+//     console.log(111);
+//     event.preventDefault();
+//     // И мы предотвращаем отправку формы путём отмены события
+//   } else {
+//     email.classList.remove('input--error')
+//     email.classList.add('input--valid')
+
+//     console.log(222);
+
+//     event.preventDefault();
+//   }
+// }, false);
