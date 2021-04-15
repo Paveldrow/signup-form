16340ca978a2978bec3b805c756bc990dd260486
Pavel Drow <paveldrow@yandex.ru>
2021-04-15 14:21:20 +0300

    fixed forms width

--- script/main.js
+++ script/main.js
@@ -3,83 +3,72 @@ const firstName = document.querySelector('input[name="first-name"]');
 const lastName = document.querySelector('input[name="last-name"]');
 const email = document.querySelector('input[name="email"]');
 const password = document.querySelector('input[name="password"]');
+const error = document.querySelector('.error-message');
 
-const regexName = /^[A-zА-яЁё]+$/;
-const regexMail = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
+const REGEXNAME = /^[A-zА-яЁё]+$/;
+const REGEXMAIL = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
 
 const errorMessage = document.createElement('span');
-errorMessage.classList.add('error-message');
+errorMessage.classList.add('invalid-message');
 
 // First Name validation
 const getValidationFirstName = () => {
-  
-
-  if (regexName.test(firstName.value)) {
-    firstName.classList.remove('input--error');
+  if (REGEXNAME.test(firstName.value)) {
+    firstName.classList.remove('input--invalid');
     firstName.classList.add('input--valid');
   } else {
-    firstName.classList.remove('input--valid');
-    firstName.classList.add('input--error');
+    errorMessage.textContent = 'First Name cannot be empty';
+    const mess = firstName.after(errorMessage.cloneNode(true));
 
-    errorMessage.textContent = "First Name cannot be empty";
-    firstName.insertAdjacentElement('afterend', errorMessage);
+    firstName.classList.add('input--invalid');
   };
 };
 
 // Last Name validation
 const getValidationLastName = () => {
-  if (regexName.test(lastName.value)) {
-    lastName.classList.remove('input--error');
+
+  if (REGEXNAME.test(lastName.value)) {
+    lastName.classList.remove('input--invalid');
     lastName.classList.add('input--valid');
   } else {
-    lastName.classList.remove('input--valid');
-    lastName.classList.add('input--error');
+    errorMessage.textContent = 'Last Name cannot be empty';
+    lastName.after(errorMessage.cloneNode(true));
 
-    // const errorMessage = document.createElement('span');
-    // errorMessage.classList.add('error-message');
-    // errorMessage.textContent = "Last Name cannot be empty";
-    // lastName.insertAdjacentElement('afterend', errorMessage);
+    lastName.classList.add('input--invalid');
   };
 };
 
 // Email validation
 const EmailValidation = () => {
-  if (regexMail.test(email.value)) {
-    email.classList.remove('input--error');
+  if (REGEXMAIL.test(email.value)) {
+    email.classList.remove('input--invalid');
     email.classList.add('input--valid');
-
   } else {
-    email.classList.remove('input--valid');
-    email.classList.add('input--error');
+    errorMessage.textContent = 'Looks like this is not an email';
+    email.after(errorMessage.cloneNode(true));
 
-    // const errorMessage = document.createElement('span');
-    // errorMessage.classList.add('error-message');
-    // errorMessage.textContent = 'Looks like this is not an email';
-    // email.insertAdjacentElement('afterend', errorMessage);
+    email.classList.add('input--invalid');
   };
 };
 
 // Password validation
 const passwordValidation = () => {
-  if (password.value == '') {
-    password.classList.remove('input--valid');
-    password.classList.add('input--error');
-
-    // const errorMessage = document.createElement('span');
-    // errorMessage.classList.add('error-message');
-    // errorMessage.textContent = 'Password cannot be empty';
-    // password.insertAdjacentElement('afterend', errorMessage);
-  } else {
-    password.classList.remove('input--error');
+  if (!password.value == '') {
+    password.classList.remove('input--invalid');
     password.classList.add('input--valid');
+  } else {
+    errorMessage.textContent = 'Password cannot be empty';
+    password.after(errorMessage.cloneNode(true));
+
+    password.classList.add('input--invalid');
   };
 };
 
 form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  errorMessage.remove();
   getValidationFirstName();
   getValidationLastName();
   EmailValidation();
   passwordValidation();
-  errorMessage.textContent='';
-  evt.preventDefault();
 });
